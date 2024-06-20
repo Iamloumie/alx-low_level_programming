eb22fb6cd49110fbae6e7a9fa8eec0cd57b70419
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-20 20:37:38 +0000

    size of data

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
