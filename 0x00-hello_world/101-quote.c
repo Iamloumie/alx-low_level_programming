a674488899ad919db73f7f632f9b46ecd5b2ca41
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-20 23:05:51 +0000

    its getting tougher

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 	*/
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
 
 	write(1, str, strlen(str) - 1);
 	return (1);
