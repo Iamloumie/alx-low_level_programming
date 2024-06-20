27b6d1d57cd2b7a1c54a37945cc25a93bd7c89ff
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-20 23:02:35 +0000

    its getting tougher

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<string.h>
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, str, strlen(str) - 1);
+	return (1);
+
+}
