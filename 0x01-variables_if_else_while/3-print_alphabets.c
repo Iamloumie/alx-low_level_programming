fc153d5d2e05deac964f852880694ac6b50a7be7
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-21 16:55:15 +0000

    I'm enjoying this

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+	*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	for (b = 65; b <= 90; b++)
+	{
+		putchar(b);
+	}
+	return (0);
+}
