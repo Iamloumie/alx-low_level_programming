84e98de97b1047b6294ff6163f30f26550c07d36
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-21 17:36:39 +0000

    its getting serious

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+	return (0);
+}
