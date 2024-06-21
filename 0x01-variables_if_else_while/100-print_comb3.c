999e06884ad8a69e620c726f59a7e93de1aded64
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-21 22:32:28 +0000

    tougher but i saw it through after so many attempts

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,18 @@ int main(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = x + 1; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
-			putchar(x + '0');
-			putchar(y + '0');
-		}
-		if (x < y)
-		{
-			putchar(',');
-			putchar(' ');
+			if (x < y)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
+
