928ecedd67703717499f09a3991287d2dbf2d8f5
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-25 16:46:05 +0000

    this is way too tough in printing the desired output

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * _isalpha - this checks the alphabetic letter
+ * @c: the incoming char
+ * Return: 0 or 1
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
