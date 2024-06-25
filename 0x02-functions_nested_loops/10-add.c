ec0445399b8b0fb79af82db36fc4ecec5ef2c656
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-25 22:51:22 +0000

    return add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result of the numbers added
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
