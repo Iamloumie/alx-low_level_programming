4724b0dc904c661bf0ff21fcf5ef8c999794879f
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-25 15:49:23 +0000

    we are getting there

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _islower - checks the lowercase alphabet in the program
+* @c: the incoming char
+* Return: returns 0 or 1
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
-
+int _islower(int c);
 
 #endif
