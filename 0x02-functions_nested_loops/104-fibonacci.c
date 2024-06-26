c10e9b389b92348e31795df2cb65660a220fc9d9
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-26 01:23:11 +0000

    last one on advanced fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int fib1 = 1, fib2 = 2;
+	int next_fib;
+
+	printf("%d, %d", fib1, fib2);
+
+	for (int i = 3; i <= 98; i++)
+	{
+		next_fib = fib1 + fib2;
+		printf(", %d", next_fib);
+
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+	printf("\n");
+
+	return (0);
+}
