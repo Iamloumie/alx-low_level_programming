3fbcf3586ca8b44c4a6106c47ca0b75ffd587560
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-26 00:09:30 +0000

    we're good to go

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int limit = 1024;
+	int sum = 0;
+
+	for (int i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("Sum of multiples of 3 or 5 below %d: %d\n", limit, sum);
+
+	return (0);
+}
