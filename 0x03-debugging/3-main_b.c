4b01c0620c92c0e7a4329442ae4861e17c4b0acb
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-26 17:40:14 +0000

    tougher than i had expected

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -13,9 +13,9 @@ int main(void)
     int day;
     int year;
 
-    month = 02;
-    day = 29;
-    year = 2000;
+    month = 1;
+    day = -12;
+    year = 1999;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
