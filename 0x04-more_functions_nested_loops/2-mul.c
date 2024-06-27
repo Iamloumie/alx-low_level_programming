624527be04af5130846eaf62f89434ade76319f9
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 11:19:18 +0000

    easy pitsy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - this multiplies two incoming digits
+ * @a: first incoming number
+ * @b: second incoming number
+ * res: displays the multiplication result
+ * Return: returns the multiplied value
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = (a * b);
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,6 +16,8 @@ void print_to_98(int n);
 void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b); 
+
 
 
 #endif
