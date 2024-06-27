07e87b8e92401f27633a2f9b9823a6c56d9f7e04
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 22:52:18 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,19 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a < 101; a++)
+	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (a % 5 == 0)
-		{
-			printf("Buzz ");
-		}
 		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
