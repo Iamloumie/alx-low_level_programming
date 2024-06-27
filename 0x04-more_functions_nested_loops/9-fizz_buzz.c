78e92568faa59863e3717994ffde49e2cf135377
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 23:42:59 +0000

    had to write the program from the scratch carefully

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-		if (i < 100)
+		if (i == 100)
 		{
-			printf(" ");
+			continue;
 		}
+		printf(" ");
 	}
 	printf("\n");
 
