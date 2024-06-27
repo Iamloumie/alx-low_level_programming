ed4af593f737b4f3824e9dab83fe56ac3adf29c7
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 12:25:30 +0000

    piece of cake

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * print_numbers - prints the integers 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
