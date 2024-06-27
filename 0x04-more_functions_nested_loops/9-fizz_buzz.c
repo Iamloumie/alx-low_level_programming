fe63d09cec3dcf00abe26366ee9deddeb76de3bf
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 22:42:27 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - look up fizzbuzz lol
+ * Return: returns 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 101; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
