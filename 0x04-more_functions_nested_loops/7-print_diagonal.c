001cd8bda66d25c34509ad713b72fc8920926a67
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-27 22:04:11 +0000

    hard but i will push through

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line with
+ * @n: the incoming length of the diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row <= size; row ++)
+	{
+		for (col = 0; col <= size; col++)
+		{
+			_putchar('#');
+		}
+	}
+		_putchar('\n');
+}
