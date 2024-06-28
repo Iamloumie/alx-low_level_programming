f6c2168e6b846dd22d57a21a25773b2890b099d8
iamloumie <lawaladedamolaolumide@gmail.com>
2024-06-28 00:25:19 +0000

    one of the toughest, don't ask me how i did it

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,18 +12,12 @@ long largest_prime_factor(long n)
 	long largest = -1;
 	long i;
 
-/**
- * Remove all factors of 2
- */
 	while (n % 2 == 0)
 	{
 		largest = 2;
 		n = n / 2;
 	}
 
-/**
- * Check for odd factors up to sqrt(n)
- */
 	for (i = 3; i <= sqrt(n); i += 2)
 	{
 		while (n % i == 0)
@@ -32,13 +26,10 @@ long largest_prime_factor(long n)
 			n = n / i;
 		}
 	}
-/**
- * If n is still greater than 2, it is prime
- */
 	if (n > 2)
 		largest = n;
 
-	return largest;
+	return (largest);
 }
 
 /**
@@ -52,5 +43,5 @@ int main(void)
 
 	printf("%ld\n", result);
 
-	return 0;
+	return (0);
 }
