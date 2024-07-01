ffceb0dc3ac0f6d677de8558c83dffc409c23499
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-01 19:10:27 +0000

    i am definitely coming back to check up on this

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: incoming string
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	end = length - 1;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
