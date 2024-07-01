7bfd70a549c545f38b5111c11f233772b3876e67
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-01 18:39:24 +0000

    i am getting tired

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: incoming string
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
