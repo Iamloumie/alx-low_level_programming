ff98013e4412f490ba51a773cdf8336f5c82e455
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-01 15:06:42 +0000

    required deep thinking

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,17 @@
 
 /**
  * swap_int - swaps the values of two integers
+ * swap: swaps the values
  * @a: first incoming integer
  * @b: second incoming integer
  */
 
 void swap_int(int *a, int *b)
 {
+	int swap;
+
+	swap = *a;
 	*a = *b;
+	*b = swap;
 }
 
