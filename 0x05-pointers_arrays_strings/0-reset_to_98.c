34993a1a9660790086a69d0e8ec78d644d9c7a77
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-01 14:31:52 +0000

    first step to C pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: incoming integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the readme file of the pointer/array/strings
