a15778f653331592a6626b4ed03106eb143b5ec7
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-01 23:27:19 +0000

    printing arrays of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,4 +7,17 @@
  * @n: incoming number of integer to be printed
  */
 
-void print_array
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+}
