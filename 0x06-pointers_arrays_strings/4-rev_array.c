5293345f9ebf10c954ddad41f8c977c0e06b77e8
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-03 20:36:22 +0000

    reverse array was like swapping

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: incoming integer
+ * @n: incoming byte
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+	int temp;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+}
