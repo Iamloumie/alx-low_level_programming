c38424c8562135bea806619d5787a0a3b8461d82
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-08 18:16:32 +0000

    still coming back to check this out

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -51,6 +51,7 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 
 
