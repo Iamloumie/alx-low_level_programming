7abb544c1e5313c9a0bffaba9ccd8df2afd35a1e
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-08 21:08:23 +0000

    matrix row and column

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -55,7 +55,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 
 #endif
