67e0e1002d26ed0b509a96d6d1e0b87d758e1fbd
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-08 15:55:43 +0000

    hard to remove strlen coz memcpy is different from string copy, there's no need for a null terminator.. it should overwrite all instead of appending

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	int dest_len = strlen(dest);
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[dest_len + i] = src[i];
+		dest[i] = src[i];
 	}
-	dest[dest_len + i] = '\0';
 	return (dest);
 }
