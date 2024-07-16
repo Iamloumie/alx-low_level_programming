8b16379299a8b1ea6f88f6ed469fe5c25336ae4e
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-16 09:50:30 +0000

    the loops are crazy, i need to visit again

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments of the program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: pointer to new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+	free(str);
+}
