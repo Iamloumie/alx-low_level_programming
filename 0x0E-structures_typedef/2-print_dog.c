8d9187d3a630b3af63273aff5b22c5eb34390737
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-22 17:29:33 +0000

    used ternary operators for simplification

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,9 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * print_dog - prints a struct dog
- * @d: pointer to the struct dog
+ * print_dog - Prints a struct dog
+ * @d: Pointer to the struct dog to print
  */
 
 void print_dog(struct dog *d)
@@ -12,31 +11,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	else
-	{
-		if (d->name != NULL)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
-			printf("Name: (nil)");
-		}
-		if (d->age >= 0)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)");
-		}
-		if (d->owner != NULL)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner: (nil)");
-		}
-	}
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
