36fee952c67e19e6b5e9d2b1b0318797a9bf71a3
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-22 19:26:34 +0000

    alloacted a new array pointer

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,31 +12,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new_array;
 	char *name_copy, *owner_copy;
 
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	new_array = malloc(sizeof(dog_t));
+	if (new_array == NULL)
 		return (NULL);
 
 	name_copy = strdup(name);
 	if (name_copy == NULL)
 	{
-		free(new_dog);
+		free(new_array);
 		return (NULL);
 	}
 	owner_copy = strdup(owner);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
-		free(new_dog);
+		free(new_array);
 		return (NULL);
 	}
 
-	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner = owner_copy;
+	new_array->name = name_copy;
+	new_array->age = age;
+	new_array->owner = owner_copy;
 
-	return (new_dog);
+	return (new_array);
 }
