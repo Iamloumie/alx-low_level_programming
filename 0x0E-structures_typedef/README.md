200b23dc4d43c82fe661bf3fe8536c00fdaa66e4
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-22 15:37:03 +0000

    technical

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+tasks on c structures and type def
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - describes a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
