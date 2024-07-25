b59f6e37e04821a7aff559510a31b0806ded237d
iamloumie <lawaladedamolaolumide@gmail.com>
2024-07-25 11:44:48 +0000

    pseudocode helped me out

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: incoming separator character
+ * @n: number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+	va_end(ap);
+}
