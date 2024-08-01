6bde8f38e7d3ce828aac350ac6b68aed6b9dc3e9
iamloumie <lawaladedamolaolumide@gmail.com>
2024-08-01 20:58:57 +0000

    betty indentation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
-/*func_b4_main- runs before the main function*/
+/**
+ * func_b4_main- runs before the main function
+ */
 
-__attribute__((constructor))
-			  void func_b4_main(void)
+void __attribute__((constructor)) func_b4_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
