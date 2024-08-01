7741edd6a54b7af93e984352ccca6a75058548b3
iamloumie <lawaladedamolaolumide@gmail.com>
2024-08-01 19:35:32 +0000

    tough but done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - creates a new node at the beginning
+ * @head: the new node head
+ * @str: the new node string
+ * Return: the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	/* i: loop for new st... @length: counts the length of new string */
+	unsigned int i, length = 0;
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,7 +23,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 
 
 
