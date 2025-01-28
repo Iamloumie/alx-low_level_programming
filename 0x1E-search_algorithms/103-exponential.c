c934b5ca0186b4c1c0beabbc1a92dcab198e2c9f
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-01-28 10:59:48 +0100

    exponential search

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,84 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * print_array - prints the array being searched
+ * @array: array being searched
+ * @left: starting index
+ * @right: ending index
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search_exp - Binary search implementation for exponential search
+ * @array: pointer to the first element of the array
+ * @left: starting index
+ * @right: ending index
+ * @value: the target value
+ * Return: Index of the value if found or -1 otherwise
+ */
+
+int binary_search_exp(int *array, size_t left, size_t right, int value)
+{
+	size_t mid;
+
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - search value in array using exponential search algo
+ * @array: the array to be searched
+ * @size: size of the array
+ * @value: the target value
+ * Return: the index of target value if found or -1 if otherwise
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1;
+	size_t right;
+
+	if (!array || size == 0)
+		return (-1);
+
+	/* Find the range for binary search */
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	/* Calculate the right boundary for the search */
+	right = (bound < size - 1) ? bound : size - 1;
+
+	/* Print the range message */
+	printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, right);
+
+	/* Perform binary search in the found range */
+	return (binary_search_exp(array, bound / 2, right, value));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,14 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+/*Advanced tasked*/
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
+int binary_search_exp(int *array, size_t left, size_t right, int value);
+int exponential_search(int *array, size_t size, int value);
+
+
 
 #endif
